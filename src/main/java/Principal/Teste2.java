4259f4cb8efc497711e25e188ac6d3c0a443671d
EduardoBotelho <edu.botelho@yahoo.com>
2024-03-07 10:39:18 -0300

    Teste 5 versao final.

--- src/main/java/Principal/Teste2.java
+++ src/main/java/Principal/Teste2.java
@@ -5,11 +5,11 @@ import java.util.concurrent.ScheduledExecutorService;
 
 public class Teste2 {
 
-    static long fibo(int n){
-        if(n < 2){
+    static long fibo(int n) {
+        if (n < 2) {
             return n;
         } else {
-            return fibo(n-1) + fibo(n-2);
+            return fibo(n - 1) + fibo(n - 2);
         }
     }
 
@@ -20,12 +20,13 @@ public class Teste2 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Entre com um numero");
         n = sc.nextInt();
-        for(int i = 0; i < n;i++){
-            System.out.println("(" + i + "):" + Teste2.fibo(i) + "\t");
+
+        for (int i = 0; i < n; i++) {
+
+                System.out.println("(" + i + "):" + Teste2.fibo(i) + "\t");
+            }
+        //if(Teste2.fibo(i) == n)
         }
-
-
 
     }
 
-}
