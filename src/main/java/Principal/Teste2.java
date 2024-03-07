563971d06bcca19641db7d07f29774658c842c28
EduardoBotelho <edu.botelho@yahoo.com>
2024-03-07 09:40:38 -0300

    Segundo Teste

--- src/main/java/Principal/Teste2.java
+++ src/main/java/Principal/Teste2.java
@@ -1,9 +1,31 @@
 package Principal;
 
-public class Teste2 {
-    public static void main(String[] args) {
+import java.util.Scanner;
+import java.util.concurrent.ScheduledExecutorService;
 
-        
+public class Teste2 {
+
+    static long fibo(int n){
+        if(n < 2){
+            return n;
+        } else {
+            return fibo(n-1) + fibo(n-2);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int n;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entre com um numero");
+        n = sc.nextInt();
+        for(int i = 0; i < n;i++){
+            System.out.println("(" + i + "):" + Teste2.fibo(i) + "\t");
+        }
+
+
+
     }
 
 }
