5ce6b8f64a6d07d4769cdfa85d49abd92bef6287
EduardoBotelho <edu.botelho@yahoo.com>
2024-03-07 09:23:38 -0300

    Initial commit

---
+++ src/main/java/Principal/Teste1.java
@@ -0,0 +1,19 @@
+package Principal;
+
+public class Teste1 {
+    public static void main(String[] args) {
+
+        int INDICE = 13;
+        int SOMA = 0;
+        int K = 0;
+
+
+        while (K<INDICE){
+            K = K+1;
+            SOMA = SOMA+K;
+        }
+        System.out.println(SOMA);
+        //calcula c = new calcula();
+
+    }
+}
---
+++ src/main/java/Principal/Teste2.java
@@ -0,0 +1,9 @@
+package Principal;
+
+public class Teste2 {
+    public static void main(String[] args) {
+
+        
+    }
+
+}
